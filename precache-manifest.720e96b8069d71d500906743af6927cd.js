71aa666939d93917a6bb5188eadfbf6b37f2814d
rampankaj89 <58067365+rampankaj89@users.noreply.github.com>
2020-01-25 19:22:42 +0530

    adobe test

---
+++ precache-manifest.720e96b8069d71d500906743af6927cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fbc2600bf68d11560f4412adb8b26e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a940433507079a1a3aa9",
+    "url": "/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "4ed591ef12e9b3c7f7d0",
+    "url": "/static/css/main.c08a442d.chunk.css"
+  },
+  {
+    "revision": "a940433507079a1a3aa9",
+    "url": "/static/js/2.fcbb9acb.chunk.js"
+  },
+  {
+    "revision": "c8307c53b5032291bed96feb4eda1573",
+    "url": "/static/js/2.fcbb9acb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ed591ef12e9b3c7f7d0",
+    "url": "/static/js/main.d339fd6c.chunk.js"
+  },
+  {
+    "revision": "69d592925aa7ff8e7d70",
+    "url": "/static/js/runtime-main.08a28377.js"
+  }
+]);
